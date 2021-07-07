4911fd1a944ba66c2dbeb419270a97707c2be6af
foundify <foundifyinfo@gmail.com>
2021-07-06 21:14:33 -0700

    Updates to home page

--- assets/dist/js/styling.js
+++ assets/dist/js/styling.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.solutions').slick({
         centerMode: true,
         slidesToShow: 9,
@@ -23,38 +23,12 @@ $(document).ready(function(){
     });
 });
 
-$(document).ready(function(){
-    $('.certifications').slick({
-        centerMode: true,
-        slidesToShow: 9,
-        slidesToScroll: 1,
-        autoplay: false,
-        draggable: false,
-        autoplaySpeed: 3000,
-        appendArrows: $('.slider__arrows'),
-        prevArrow: '<div style="font-size: 30px !important;" class="mx-3 slider__arrow slider__arrow_dir_left">←</div>',
-        nextArrow: '<div style="font-size: 30px !important;" class="mx-3 slider__arrow slider__arrow_dir_right">→</div>',
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 5
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
 
 $(document).ready(function () {
     $(".navbar-toggler").on("click", function () {
-          $(".open").toggleClass("hidden");
-          $(".open").toggleClass("visible");
-          $(".close").toggleClass("visible");
-          $(".close").toggleClass("hidden");
+        $(".open").toggleClass("hidden");
+        $(".open").toggleClass("visible");
+        $(".close").toggleClass("visible");
+        $(".close").toggleClass("hidden");
     });
 });
