de36c377d039cd5a2928181c3ab442bc6133f336
foundify <foundifyinfo@gmail.com>
2021-07-11 15:10:28 -0700

    Added solutions.html

--- assets/dist/js/styling.js
+++ assets/dist/js/styling.js
@@ -23,6 +23,47 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $('.solutions-nav').slick({
+        centerMode: true,
+        slidesToShow: 9,
+        autoplay: false,
+        swipeToSlide: true,
+        arrows: false,
+        dots: false,
+        speed:600,
+        focusOnSelect: true,
+        draggable: true,
+        asNavFor: '.solutions-content',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 5
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+$(document).ready(function () {
+    $('.solutions-content').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        swipeToSlide: true,
+        arrows: false,
+        speed:600,
+        dots: false,
+        draggable: false,
+        asNavFor: '.solutions-nav',
+    });
+});
+
 
 $(document).ready(function () {
     $(".navbar-toggler").on("click", function () {
